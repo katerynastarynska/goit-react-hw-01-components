a9149cb9244ad881e26600e96bc5130de609e835
katerynastarynska <katerynastarynska@gmail.com>
2022-09-20 22:43:24 -0500

    add styles to profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,16 @@
 import Profile from "./Profile";
 import user from "../user.json";
+import 'normalize.css';
+import css from "./App.module.css";
+
 export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <div>
-        <Profile username={user.username}
+    <div classname={css.card}>
+      <Profile username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats} />
-              
-      </div>
-      React homework template!!!
     </div>
-
   );
 }; 
